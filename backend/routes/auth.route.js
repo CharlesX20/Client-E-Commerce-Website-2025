ab3364e38a631cf9660fe66e2de33d45810ae496
CharlesGPT <chimecharles23@gmail.com>
2025-09-19 03:55:17 -0400

    Done for today would continue tomorrow

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { login, logout, signup, refreshToken } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", signup);
+router.post("/signup", signup);
 
-router.get("/login", login);
+router.post("/login", login);
 
-router.get("/logout", logout);
+router.post("/logout", logout);
+
+router.post("/refresh-token", refreshToken);
+
+//WORK ON THIS LATER
+//router.get("/profile", getProfile);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
+
+import {connectDB} from "./lib/db.js";
 
 
 dotenv.config();
@@ -8,10 +12,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json()); //allows you to pass the body of the request
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+    console.log("Server is running on http://localhost:" + PORT);
+    connectDB();
 })
 
-//kZZxrPtwSj1eNc47
